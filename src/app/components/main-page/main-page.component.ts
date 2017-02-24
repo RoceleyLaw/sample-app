cae307b5b558934f9e93d5fa32338d397e67c87b
roceleylaw <roceleylaw@gmail.com>
2017-02-24 10:30:37 -0500

    editing index page

---
+++ src/app/components/main-page/main-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'main-page',
+  templateUrl: 'main-page.component.html',
+  styleUrls: [ 'main-page.sass' ]
+})
+export class MainPageComponent {
+    constructor() { }
+}
